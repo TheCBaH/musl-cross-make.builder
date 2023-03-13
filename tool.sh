92fdcb03b8bd349c76470995cc26de2995025e71
Vladimir N. Silyaev <vsilyaev@gmail.com>
2023-03-13 04:28:54 +0000

    Save meta-information in the toolchain package

--- tool.sh
+++ tool.sh
@@ -9,6 +9,15 @@ arch=$(echo $target|cut -d- -f 1)
 kind=$(echo $target|sed -e 's/.*musl\(eabi\)\{0,1\}//')
 lib_root=$dst/$target
 lib=$dst/$target/lib
+case "$arch" in
+powerpc*)
+    qemu_arch="ppc${arch#powerpc}"
+    ;;
+*)
+    qemu_arch="$arch"
+    ;;
+esac
+
 case "$cmd" in
 fixup)
     ld=ld-musl-${arch}${kind}.so.1
@@ -18,15 +27,20 @@ fixup)
     ln -s $link $lib/$ld
     ;;
 qemu-arch)
-    case "$arch" in
-    powerpc*)
-        arch="ppc${arch#powerpc}"
-        ;;
-    esac
-    echo $arch
+    echo $qemu_arch
     ;;
 lib)
     echo $lib_root
+    ;;
+meta)
+    grep '^GCC_VER' musl-cross-make/config.mak >$dst/.config
+    grep '^BINUTILS_VER\|^MUSL_VER\|^GMP_VER\|^MPC_VER\|^MPFR_REV\|^LINUX_VER' musl-cross-make/Makefile >>$dst.config
+    cat <<_EOF_ >$dst/with-target
+#!/bin/sh
+set -eu
+exec qemu-$qemu_arch -L $lib_root \$@
+_EOF_
+    chmod +x $dst/with-target
     ;;
 *)
     echo "Not supported '$cmd'" >&2
